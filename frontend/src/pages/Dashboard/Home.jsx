e1c3a17a91f9d82a655ea70a1dbc06d553943630
Tushar <tushartalekar2024@gmail.com>
2025-09-19 19:32:07 +0530

    Recent Transaction and Finance Overview

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -9,6 +9,7 @@ import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
 import InfoCard from '../../components/Cards/InfoCard';
 import { addThousandsSeparator } from '../../utils/helper';
 import RecentTransaction from '../../components/Dashboard/RecentTransaction';
+import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 
 const Home = () => {
   useUserAuth();
@@ -45,7 +46,7 @@ const Home = () => {
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <InfoCard
             icon={<IoMdCard />}
             label="Total Balance"
@@ -66,12 +67,19 @@ const Home = () => {
             value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
             color="bg-red-500"
           />
-        </div>
+        </div> */}
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
           <RecentTransaction
             transactions={dashboardData?.recentTransactions}
-            onSeeMore={()=>navigate("/expense")}
+            onSeeMore={() => navigate("/expense")}
+          />
+
+          <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpense={dashboardData?.totalExpense || 0}
+
           />
         </div>
       </div>
