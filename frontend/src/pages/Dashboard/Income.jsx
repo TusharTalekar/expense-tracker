dfc321e90b4f90bd12fe4ad39733cbfcf03d675d
Tushar <tushartalekar2024@gmail.com>
2025-09-27 17:14:26 +0530

    IncomeOverview

--- frontend/src/pages/Dashboard/Income.jsx
+++ frontend/src/pages/Dashboard/Income.jsx
@@ -1,10 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashboardLayout from '../../components/layouts/DashboardLayout'
+import IncomeOverview from '../../components/Income/IncomeOverview';
+import { } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosinstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import Modal from '../../components/Modal';
 
 const Income = () => {
+  const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
+
+  const [incomeData, setIncomeData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+  });
+
+
+  // get all income details
+  const fetchIncomeDetails = async () => {
+    if (loading) return;
+    setLoading(true);
+    try {
+      const response = await axiosInstance.get(`${API_PATHS.INCOME.GET_ALL_INCOME}`);
+      if (response.data) {
+        setIncomeData(response.data);
+      }
+    } catch (err) {
+      console.log("Something went wrong , try again");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // handle add income 
+  const handleAddIncome = async (income) => {
+
+  };
+
+  // delete income 
+  const deleteIncome = (id) => {
+
+  };
+  // handle download 
+  const handleDownloadIncomeDetails = () => {
+
+  }
+
+  useEffect(() => {
+    fetchIncomeDetails();
+    return () => { };
+  }, []);
+
   return (
-    <div>
-      Income
-    </div>
+    <DashboardLayout activeMenu="Income">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1">
+          <div className="">
+            <IncomeOverview
+              transactions={incomeData}
+              onAddIncome={() => setOpenAddIncomeModal(true)}
+            />
+          </div>
+        </div>
+        <Modal
+          isOpen={openAddIncomeModal}
+          onClose={() => setOpenAddIncomeModal(false)}
+          title="Add income"
+        >
+          <div>Add Income Form</div>
+        </Modal>
+
+      </div>
+    </DashboardLayout>
+
   )
 }
 
--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,3 +1,5 @@
+import moment, { } from "moment";
+
 export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
@@ -24,11 +26,22 @@ export const addThousandsSeparator = (num) => {
     return fractionalPart ? `${formattedInteger}.${fractionalPart}` : formattedInteger;
 };
 
-export const prepareExpenseBarChartData=(data=[])=>{
-    const chartData=data.map((item)=>({
-        category:item?.category,
-        amount:item?.amount,
+export const prepareExpenseBarChartData = (data = []) => {
+    const chartData = data.map((item) => ({
+        category: item?.category,
+        amount: item?.amount,
     }));
-     
-    return chartData; 
+
+    return chartData;
+}
+
+export const prepareIncomeBarChartData = (data = []) => {
+    const sortedData = [...data].sort((a, b) => new Date(a.Date) - new Date(b.Date));
+    const chartData = sortedData.map((item) => ({
+        month: moment(item?.date).format("Do MMM"),
+        amount: item?.amount,
+        source: item?.source,
+    }));
+
+    return chartData;
 }
