056130609b967dbfa268706b54998b1d4a7fa5b0
Tushar <tushartalekar2024@gmail.com>
2025-09-27 17:13:57 +0530

    CustomerBarChart for source

--- frontend/src/components/Charts/CustomBarChart.jsx
+++ frontend/src/components/Charts/CustomBarChart.jsx
@@ -11,7 +11,7 @@ const CustomBarChart = ({ data }) => {
             return (
                 <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300">
                     <p className="text-xs font-semibold text-purple-800 mb-1">
-                        {payload[0].payload.category}
+                        {payload[0].payload.category || payload[0].payload.source}
                     </p>
                     <p className="text-sm text-gray-600">
                         Amount: <span className="text-sm font-medium text-gray-900 ">${payload[0].payload.amount}</span>
@@ -27,8 +27,13 @@ const CustomBarChart = ({ data }) => {
                 <BarChart data={data}>
                     <CartesianGrid stroke="none" />
 
-                    <XAxis dataKey={"category"} tick={{ fontSize: 12, fill: "#555" }} stroke="none" />
-                    <YAxis tick={{ fontSize: 12, fill: "#555" }} stroke="none" />
+                    <XAxis dataKey={data.length && data[0]?.category ? "category" : "source"}
+                        tick={{ fontSize: 12, fill: "#555" }}
+                        stroke="none" />
+
+                    <YAxis
+                        tick={{ fontSize: 12, fill: "#555" }}
+                        stroke="none" />
 
                     <Tooltip content={CustomToolTip} />
 
@@ -36,8 +41,8 @@ const CustomBarChart = ({ data }) => {
                         dataKey={"amount"}
                         fill="#FF8042"
                         radius={[10, 10, 0, 0]}
-                        // activeDot={{ r: 8, fill: "yellow" }}
-                        // activeStyle={{ fill: "green" }}
+                    // activeDot={{ r: 8, fill: "yellow" }}
+                    // activeStyle={{ fill: "green" }}
                     >
                         {data.map((entry, index) => (
                             <Cell key={index} fill={getBarColor(index)} />
