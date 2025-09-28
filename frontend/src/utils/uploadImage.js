6e9e9809f9a8869d8081f43bbaa3ca89d700db61
Tushar <tushartalekar2024@gmail.com>
2025-09-28 19:31:06 +0530

    image upload type

--- frontend/src/utils/uploadImage.js
+++ frontend/src/utils/uploadImage.js
@@ -10,7 +10,8 @@ const uploadImage = async (imageFile) => {
     try {
         const response = await axiosInstance.post(API_PATHS.IMAGE.UPLOAD_IMAGE, formData, {
             headers: {
-                'Content-Type': 'multipart/form-data',
+                'Content-Type': 'multipart/form-data', 
+                'Content-Type': undefined,
             },
         });
         return response.data;
