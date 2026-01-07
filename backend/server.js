109d854c6afdd1497a3174603da5191c7164341c
TusharTalekar <tushartalekar2024@gmail.com>
2026-01-07 12:22:30 +0530

    fix:cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const app = express();
 // middelware to handle cors
 app.use(cors({
     origin: [
-        "*"
+        "https://finflow-peach.vercel.app",
+        "https://localhost:5000",
+        "https://localhost:5173",
+        "https://localhost:3000",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
